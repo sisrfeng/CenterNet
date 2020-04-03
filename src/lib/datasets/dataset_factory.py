f54a717cca9bc0acf4c66b7ae42788c0c524a556
CV beginner <53520949+sisrfeng@users.noreply.github.com>
2020-04-03 15:12:54 +0800

    Update dataset_factory.py

--- src/lib/datasets/dataset_factory.py
+++ src/lib/datasets/dataset_factory.py
@@ -11,13 +11,15 @@ from .dataset.coco import COCO
 from .dataset.pascal import PascalVOC
 from .dataset.kitti import KITTI
 from .dataset.coco_hp import COCOHP
+from .dataset.aic import AIC
 
 
 dataset_factory = {
   'coco': COCO,
   'pascal': PascalVOC,
   'kitti': KITTI,
-  'coco_hp': COCOHP
+  'coco_hp': COCOHP,
+  'aic': AIC
 }
 
 _sample_factory = {
